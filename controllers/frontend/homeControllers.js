454d31d1d94e8d337bbf5bb604d502db0d1c52ff
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2024-03-11 07:57:46 +0600

    Add Shop model and retrieve featured shops in getHomeData

--- controllers/frontend/homeControllers.js
+++ controllers/frontend/homeControllers.js
@@ -1,4 +1,5 @@
 const Category = require('../../models/common/categoryModel');
+const Shop = require('../../models/seller/shopModel');
 const sendResponse = require('../../utils/sendResponse');
 const getHomeData=async(req,res)=>{
     // get 10 categories, descending of sortValue
@@ -6,8 +7,17 @@ const getHomeData=async(req,res)=>{
         limit: 10,
         order: [['sortValue', 'DESC']]
     });
+    const shops = await Shop.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        // include: {
+        //     model: Category,
+        //     attributes: ['name']
+        // }
+    });
     const data={
-        categories
+        categories,
+        featuredShops:shops
     
     }
     return sendResponse(res, 200, true, 'Home data retrieved successfully', data);
