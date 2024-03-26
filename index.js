808968f5f4fb296a803025c553164d7d8510ac81
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2024-03-26 14:49:21 +0600

    msg

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const db=require('./db_config/db');
-const sellerRoutes=require('./routes/seller/sellerRoutes');
-const categoryRoutes=require('./routes/seller/categoryRoutes');
-const apiRoutes=require('./routes/api/apiRoutes');
+const Order=require('./models/common/orderModel');
+const OrderItem=require('./models/common/orderItems');
+// const sellerRoutes=require('./routes/seller/sellerRoutes');
+// const categoryRoutes=require('./routes/seller/categoryRoutes');
+// const apiRoutes=require('./routes/api/apiRoutes');
 require('./models/associations/ProductCategories');
 require('./models/associations/ProductImages');
 require('./models/associations/ProductAttributeValue');
@@ -25,15 +27,15 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(express.json()); 
 app.use(cors());
-app.use("/seller",sellerRoutes);
-app.use("/seller/category",categoryRoutes);
-app.use("/api",apiRoutes);
+// app.use("/seller",sellerRoutes);
+// app.use("/seller/category",categoryRoutes);
+// app.use("/api",apiRoutes);
 
-// uploads folder accessible
+
 app.use('/uploads', express.static('uploads'));
-// Define a route
+
 app.get('/', (req, res) => {
-  res.send('Hello World!'); // Respond with 'Hello World!' when someone accesses the root URL
+  res.send('Hello World!'); 
 });
 
 // Start the server
