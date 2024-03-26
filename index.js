1d4088bb421a9d7b004542e41f152557239c7b94
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2024-03-26 23:48:26 +0600

    msg

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const db=require('./db_config/db');
-const OrderItems=require('./models/common/orderItems')
-// const sellerRoutes=require('./routes/seller/sellerRoutes');
-// const categoryRoutes=require('./routes/seller/categoryRoutes');
-// const apiRoutes=require('./routes/api/apiRoutes');
-// require('./models/associations/ProductCategories');
-// require('./models/associations/ProductImages');
-// require('./models/associations/ProductAttributeValue');
-// require('./models/associations/OrderItemProduct');
-// require('./models/associations/ProductShop');
+const sellerRoutes=require('./routes/seller/sellerRoutes');
+const categoryRoutes=require('./routes/seller/categoryRoutes');
+const apiRoutes=require('./routes/api/apiRoutes');
+require('./models/associations/ProductCategories');
+require('./models/associations/ProductImages');
+require('./models/associations/ProductAttributeValue');
+require('./models/associations/OrderItemProduct');
+require('./models/associations/ProductShop');
 
 db.sync({alter:true})
   .then(() => {
@@ -24,11 +23,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-// app.use(express.json()); 
-// app.use(cors());
-// app.use("/seller",sellerRoutes);
-// app.use("/seller/category",categoryRoutes);
-// app.use("/api",apiRoutes);
+app.use(express.json()); 
+app.use(cors());
+app.use("/seller",sellerRoutes);
+app.use("/seller/category",categoryRoutes);
+app.use("/api",apiRoutes);
 
 // uploads folder accessible
 app.use('/uploads', express.static('uploads'));
